237392178851ad200640ebdd348792cc2770f62f
Om Tajane <omtajane26@gmail.com>
2026-01-23 12:01:34 +0530

    subarray

---
+++ PrintSubArray.java
@@ -0,0 +1,25 @@
+public class PrintSubArray 
+{
+    public static void printSubArray(int num[])
+    {
+        for(int i =0; i<num.length; i++)
+        {   //starting index, starting with 0 that is 2 
+            int start = i;
+            for(int j = i; j<num.length; j++)
+            {   //this is the ending index value 
+                int end = j;
+                for(int k=start; k<=end ; k++)
+                {
+                    System.out.print(num[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) 
+    {
+        int number[] = {2, 4, 6, 8, 10};
+        printSubArray(number);
+    }    
+}
